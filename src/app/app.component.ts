1be63e9ac3d100f06d3d13e190c8001a81f2688b
Roberto Mowers <roberto.mowerji@anahuac.mx>
2025-02-15 15:04:39 -0600

    cambios en rutas y app.component para el manejo de lado publico y administrativo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,10 @@
 import { Component } from '@angular/core';
-import { FooterComponent } from './components/footer/footer.component';
-import { NavbarComponent } from './components/navbar/navbar.component';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   imports: [
-    NavbarComponent,
-    FooterComponent, 
     RouterOutlet
   ],
   styleUrls: ['./app.component.css']
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,13 +1,39 @@
 import { Routes } from '@angular/router';
+import { PublicLayoutComponent } from './layouts/public-layout/public-layout.component';
+import { AdminLayoutComponent } from './layouts/admin-layout/admin-layout.component';
+
+
+//Rutas Publicas
+import { HomePageComponent } from './pages/home/home.component';
 import { ExploreComponent } from './pages/explore/explore.component';
 import { StoreComponent } from './pages/store/store.component';
-import { HomePageComponent } from './pages/home/home.component';
 import { MenusComponent } from './pages/menus/menus.component';
 
+// Rutas de Admin
+import { LoginComponent } from './admin/pages/login/login.component';
+import { DashboardComponent } from './admin/pages/dashboard/dashboard.component';
+import { authGuard } from './services/guard/auth.guard';
+
 export const routes: Routes = [
-    { path: '', component: HomePageComponent },
-    { path: 'home', component: HomePageComponent },
-    { path: 'store', component: StoreComponent },
-    { path: 'explore', component: ExploreComponent },
-    { path: 'menus', component: MenusComponent },
+    {
+        path: '',
+        component: PublicLayoutComponent,
+        children: [
+            { path: '', component: HomePageComponent },
+            { path: 'home', component: HomePageComponent },
+            { path: 'store', component: StoreComponent },
+            { path: 'explore', component: ExploreComponent },
+            { path: 'menus', component: MenusComponent }
+        ]
+    },
+    {
+        path: 'admin',
+        component: AdminLayoutComponent,
+        children: [
+            { path: 'dashboard', component: DashboardComponent, canActivate: [authGuard] }
+        ]
+    },
+    { path: 'login', component: LoginComponent },
+    { path: '**', redirectTo: '' }
 ];
+
