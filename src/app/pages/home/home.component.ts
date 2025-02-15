db40f33433095ebef66958d38edae4a4fbfa1fc5
Roberto Mowers <roberto.mowerji@anahuac.mx>
2025-02-15 15:02:06 -0600

    cambios visuales en la pagina principal

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,13 +1,13 @@
 import { Component } from '@angular/core';
 import { HomeMainContentComponent } from './components/first/home-main-content/home-main-content.component';
 import { NewHomeContentComponent } from './components/first/new-home-content/new-home-content.component';
-import { HomeProjectsContentComponent } from './components/first/home-projects-content/home-projects-content.component';
 import { HomeSectionsContentComponent } from './components/second/home-sections-content/home-sections-content.component';
 import { HomePromoContentComponent } from './components/third/home-promo-content/home-promo-content.component';
+import { OptionsContainerComponent } from '../../shared/options-container/options-container.component';
 
 @Component({
   selector: 'app-home-component',
-  imports:  [HomeMainContentComponent, HomeSectionsContentComponent, NewHomeContentComponent, HomeProjectsContentComponent, HomePromoContentComponent],
+  imports:  [HomeMainContentComponent, HomeSectionsContentComponent, NewHomeContentComponent, OptionsContainerComponent, HomePromoContentComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
