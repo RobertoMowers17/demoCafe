322de2d8121556149b706b8194fac12c0514ec40
Roberto Mowers <roberto.mowerji@anahuac.mx>
2025-02-15 15:00:02 -0600

    Creacion de elementos layout-public y layout-admin para control de paginas publicas y privada

---
+++ src/app/layouts/public-layout/public-layout.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { NavbarComponent } from '../../components/navbar/navbar.component';
+import { FooterComponent } from '../../components/footer/footer.component';
+
+@Component({
+  selector: 'app-public-layout',
+  imports: [
+    RouterOutlet,
+    NavbarComponent,
+    FooterComponent
+  ],
+  templateUrl: './public-layout.component.html',
+  styleUrl: './public-layout.component.css'
+})
+export class PublicLayoutComponent {
+
+}
